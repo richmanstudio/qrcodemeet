fbb115e0b5367ddf0e4f4a82a8421ee84f8f8a40
richmanstudio <133752573+richmanstudio@users.noreply.github.com>
2025-06-12 11:59:32 +1000

    sdss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Замените <repo-name> на фактическое имя вашего репозитория
-const repoName = 'qrcodemeet';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [react()],
+  // Для Project Page на GitHub Pages укажите здесь имя вашего репозитория —
+  // подставьте точно то, что вы видите в URL:
+  // https://<ваш-логин>.github.io/qrcodemeet/
+  base: '/qrcodemeet/',
+
+  plugins: [
+    react(), // поддержка JSX/TSX и React Fast Refresh
+  ],
 });
